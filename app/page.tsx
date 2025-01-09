f586520b513b26011fb0dd4abaef91896e30c985
kkomin <100822055+kkomin@users.noreply.github.com>
2025-01-09 17:33:05 +0900

    [DESIGN] 🎨 : TitleForm.tsx 반응형 웹 디자인으로 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import { TitleForm } from "@/components/auth/TitleForm";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <TitleForm/>
       메인페이지 yo
     </main>
--- components/auth/TitleForm.tsx
+++ components/auth/TitleForm.tsx
@@ -1,7 +1,24 @@
+"use client"
+import { useEffect, useState } from "react";
+
 export function TitleForm() {
+    const [imgSrc, setImageSrc] = useState('/img/doit.png');
+
+    useEffect(() => {
+        const handleResize = () => {
+            const isMobile = window.innerWidth <= 480;
+            setImageSrc(isMobile ? 'img/image.png' : '/img/doit.png');
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    });
+
     return (
         <div className="title-container">
-            <img className="title-img-doit" src="/img/doit.png">
+            <img className="title-img-doit" src={imgSrc}>
             </img>
         </div>
     );
